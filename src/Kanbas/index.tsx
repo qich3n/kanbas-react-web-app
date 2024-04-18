4e41093d06356fa0cde28cdc362b1331b35556b5
qich3n <chen.qi3@northeastern.edu>
2024-04-18 16:50:22 -0400

    mongoose

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -11,7 +11,7 @@ import Account from "./Account";
 const API_BASE = process.env.REACT_APP_API_BASE;
 function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
-    const COURSES_API = `${API_BASE}/api/courses`;
+    const COURSES_API = "https://kanbas-node-server-app-a6-90b9.onrender.com/api/courses";
     const findAllCourses = async () => {
         const response = await axios.get(COURSES_API);
         setCourses(response.data);
